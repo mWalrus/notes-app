0ede3aad03876c2c86df5e62a35ffa681644aa05
mWalrus <hellkvistoskar@gmail.com>
2020-12-04 11:35:22 +0100

    adding header

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 className='main-head'>Notes</h1>
+        <p className='sub-class'>
+          A simple notes application for everyday thoughts
+        </p>
+      </header>
+    )
+  }
+}
